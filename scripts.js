fb3639ef687211307929cd7aa4e767b757123b94
Breana Knight <Akewlbreeze.bc@gmail.com>
2016-12-12 17:08:27 -0700

    a commit to stash

--- scripts.js
+++ scripts.js
@@ -2,24 +2,4 @@ var userWebsiteTitle = document.querySelector(".user-input-title");
 var userWebsiteUrl = document.querySelector(".user-input-url");
 var userEnterButton = document.querySelector(".enter-button");
 
-console.log("hi")
-
-// window.onload = function(){
-//   disableInputFeilds();
-// }
-
-
-// function disableInputFeilds(){
-//   userWebsiteTitle.disabled = true;
-//   userWebsiteUrl.disabled = true;
-//   userEnterButton.disabled = true;
-// }
-
-function userInputValues(){
-  var display = userWebsiteTitle.text;
-  console.log(display);
-}
-
-function collectUserTitle(){
-  var title = userWebsiteTitle.text;
-}
+var 
