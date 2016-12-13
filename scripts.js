cbb14f69ebc1ec87d3f8f6feab2a317f40c760cc
Breana Knight <Akewlbreeze.bc@gmail.com>
2016-12-12 18:01:46 -0700

    adding switches to jquery

--- scripts.js
+++ scripts.js
@@ -1,26 +1,31 @@
-var userWebsiteTitle = document.querySelector(".user-input-title");
-var userWebsiteUrl = document.querySelector(".user-input-url");
-var userEnterButton = document.querySelector(".enter-button");
+var userWebsiteTitle = $(".user-input-title");
+var userWebsiteUrl = $(".user-input-url");
+var userEnterButton = $(".enter-button");
 
-var websiteTitleInput = document.querySelector(".title-underline");
-var websiteUrlInput = document.querySelector(".url-underline");
-console.log("hi")
+var websiteTitleInput = $(".title-underline");
+var websiteUrlInput = $(".url-underline");
+
+var bookmarkContent = "<div class='bookmark-card'>" + "<h4 class='title-underline'>The Website title</h4>" + "<hr>"  + "<h4 class = 'url-underline'> www.thewebsiteurl.com</h4>" + "<hr>" + "<span class = 'read'> read</span>" + "<span class = 'delete'>delete</span>" + "</div>";
 
 
-userEnterButton.addEventListener("click", function(){
+$('button').on("click", function(){
+  $(".right-section").prepend(bookmarkContent);
 
-  enterUserTitle();
+});
+
+// get the user submited info and assign to a variable and use ${} selection in the concatenation
+
+$("userEnterButton").on("click", function(){
+  var title = enterUserTitle();
   enterUserUrl();
 });
 
 function enterUserTitle(){
-  var display = userWebsiteTitle.value;
-  websiteTitleInput.innerText = display;
-  console.log(display);
+  var display = $(".userWebsiteTitle").val();
+  $("websiteTitleInput").text("display");
 }
 
 function enterUserUrl(){
-  var displayUrl = userWebsiteUrl.value;
-  websiteUrlInput.innerText = displayUrl;
-  console.log(displayUrl);
+  var displayUrl = $("userWebsiteUrl").val();
+  $("websiteUrlInput").text("displayUrl");
 }
